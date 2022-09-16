41aac889685f0f2862865b47d2dcf0a6ad0954a8
Stefan Munnes <munnes@wzb.eu>
2022-09-16 14:13:09 +0200

    change o fsum regex

--- scripts/bt_prep_protocolls.R
+++ scripts/bt_prep_protocolls.R
@@ -44,20 +44,17 @@ remove_header <- function(text_raw = text, rex_name = regex_n) {
 
   # remove main part of header (Deutscher Bundestag, session, place, date)
   regex_header_base <- regex("
-    ([0-9]{1,5}\\.*?)?        # number of Wahlperiode
-    \\s*Deutscher\\sBundestag # base name
-    .*?Sitzung                # number of session
-    .*?[0-9]{4,4}             # date till year
-    ", comments = TRUE)
-
-  text_raw <- str_replace_all(text_raw, regex_header_base, "\nHEADER\n")
-
+    ([0-9]{1,5}\\.{1,5})*?        # number of Wahlperiode
+    \\s*Deutscher\\sBundestag     # base name
+    .{1,30}?Sitzung               # number of session
+    .{0,50}?[0-9]{4,4}            # date till year
+    ", comments = TRUE, dotall = TRUE)
 
   # remove page numbering around header (front or back)
   regex_header_page <- regex("
-    ([0-9]{1,6}[.,\\s]{1,7})? # page num & space/dot/pagebreak before header
+    ([0-9]{1,6}[.,\\s]{1,8})? # page num & space/dot/pagebreak before header
     HEADER
-    ([.,\\s]{1,7}[0-9]{1,7})? # page num & space/dot/pagebreak behind header
+    ([.,\\s]{1,7}[0-9]{1,8})? # page num & space/dot/pagebreak behind header
     ", comments = TRUE)
 
   text_raw <- str_replace_all(text_raw, regex_header_page, "\nHEADER\n")
@@ -74,26 +71,28 @@ remove_header <- function(text_raw = text, rex_name = regex_n) {
 
   # remove name of speaker under/above header if splited speech
 
-  # names behind header: e.g.HEADER\n Pau; (Pau); (Pau [Berlin]);  Pau (Berlin)
-  text_raw <- gsub(
-    paste0(
-      "\\n+HEADER\\s+.{0,30}?",
-      "(", rex_name, ")",
-      "(?!.*:)",
-      "((\\s(\\[|\\().*?(\\]|\\)))*\\)*\\s*)*"
-    ),
-    "\nHEADER\n",
-    text_raw,
-    perl = TRUE
-  )
+  if (wp < 18) { # WP 1- WP 17: names behind header:
+    # e.g. HEADER\n Pau; (Pau); (Pau [Berlin]);  Pau (Berlin)
+    text_raw <- gsub(
+      paste0(
+        "\\n+HEADER\\s+.{0,30}?",
+        "(", rex_name, ")",
+        "(?!.*(:|Staatssekretär))", # anything behind name except speaker signs
+        "((\\s(\\[|\\().*?(\\]|\\)))*\\)*\\s*)*"
+      ),
+      "\nHEADER\n",
+      text_raw,
+      perl = TRUE
+    )
+  } else { # names in front of header: e.g. \nDr. Anton Hofreiter\nHEADER
+    text_raw <- gsub(
+      paste0("\\n.{0,30}(", rex_name, ").{0,20}\\n+HEADER\\n+"),
+      "\nHEADER\n",
+      text_raw,
+      perl = TRUE
+    )
+  }
 
-  # names in front of header: e.g. \nDr. Anton Hofreiter\nHEADER
-  text_raw <- gsub(
-    paste0("\\n.{0,30}(", rex_name, ").{0,20}\\n+HEADER\\n+"),
-    "\nHEADER\n",
-    text_raw,
-    perl = TRUE
-  )
 
   # remove HEADER
   text_nohead <- str_remove_all(text_raw, "HEADER")
@@ -102,9 +101,12 @@ remove_header <- function(text_raw = text, rex_name = regex_n) {
 }
 
 
-
-
-regex_1 <- "^.*?((Die Sitzung wird.+?eröffnet\\.)|(Beginn:.*?Uhr))\\s{0,2}(\\n)*(Vizepräsident|Präsident)"
+regex_start <- regex(
+  "^.*?
+  (Die\\sSitzung\\swird.+?eröffnet\\.|Beginn:.*?Uhr\\.*)\\s{0,4}
+  ((Vize)*[Pp]räsident)",
+  comments = TRUE, dotall = TRUE
+)
 
 
 # init correct Wahlperiode and names
@@ -126,8 +128,8 @@ test <- lapply(seq_len(nrow(pt_smpl)), function(row) { # seq_len(nrow(pt_smpl))
   text <- pt_smpl$text[row]
 
   # remove all stuff befor begin (e.g table of content)
-  if (grepl(regex_1, text)) {
-    text <- gsub(regex_1, "\\5", text)
+  if (str_detect(text, regex_start)) {
+    text <- str_replace(text, regex_start, "\\2")
   } else { # WP 14: zT ist Beginn verutscht auf die 2. Seite, mitten im Text
 
     text <- gsub(
@@ -172,14 +174,13 @@ test <- lapply(seq_len(nrow(pt_smpl)), function(row) { # seq_len(nrow(pt_smpl))
     "\nZWISCHENRUF\n"
   )
 
-
   #  speaker: begin of speech: from newline to : with name (keywords/paranth.)
-  regex_speaker <- stringr::regex(
+  regex_speaker <- regex(
     paste0(
       "\\n(", # begin with linebreak and first group
       "[^\\n]{0,30}?", # take everything before, without linebreak
       "(", regex_n, ")", # member names of Wahlperiode
-      ".{0,10}?", # space/comma after name, before additional words
+      "[^\\n]{0,25}?", # space/comma after name,before additional words(?!.*\\2)
       "(((Minister|Bundesminister|Staatssekretär).{0,45}?)|", # longer keywords
       "(Antragsteller|Anfragender|Berichterstatter|Schriftführer|", # keywords
       "(\\(.{0,35}\\))).{0,5})?", # filled parantheses (city/party)
@@ -198,12 +199,16 @@ test <- lapply(seq_len(nrow(pt_smpl)), function(row) { # seq_len(nrow(pt_smpl))
 saveRDS(test, file = "../data/tmp_smpl.RDS")
 test <- readRDS("../data/tmp_smpl.RDS")
 
-c <- test[[1]]
+c <- test[[2]]
 View(c)
 
-fileConn <- file("output.txt")
-writeLines(test[[10]], fileConn) # test[[1]]  df_smpl$text[15]
+fileConn <- file("../data/tmp_smpl.txt")
+writeLines(test[[18]], fileConn) # test[[1]]  df_smpl$text[15]
+#writeLines(pt_smpl$text[[3]], fileConn) # test[[1]]  df_smpl$text[15]
 close(fileConn)
+
+
+# ! check: text 38: Amira Mohamed Ali
 
 
 Text 10: Dr. von Manger-Koenig, Staatssekret�r im Bundesministerium f�r Gesundheitswesen
@@ -269,111 +274,3 @@ test5 <- kwic(tokens(corpus(unlist(test))), "Staatssek*", window = 15)
 # Dr. Rainer Kraft
 # Parl. Staatssekretärin Rita Schwarzelühr-Sutter
 # Parl Staatssekretärin bei der Bundesministerin für Umwelt, Naturschutz, Bau und Reaktorsicherheit
-
-
-
-# in Text
-
-# WP 4
-# Staatssekretär Dr. Lippe rt (ANSTELLE VON LIPPERT)
-
-# WP 14
-
-# Frage stellen: Wie muss die Energiepolitik im 21. Jahr-
-#
-#  Präsident Wolfgang Thierse
-#
-# Deutscher Bundestag – 14. Wahlperiode – 95. Sitzung. Berlin, Donnerstag, den 23. März 2000 8735
-#
-# (A)
-
-# https://dserver.bundestag.de/btp/14/14057.pdf
-# Vizepräsident Dr. Hermann Otto Solms
-
-
-
-# WP 19
-
-# 3964 	Deutscher Bundestag — 4. Wahlperiode — 82. Sitzung. Bonn, Donnerstag, den 27. Juni 1963
-# Staatssekretär Dr. Seiermann
-
-
-
-a <- df_smpl$text[7]
-
-# WP 1:
-
-# remove linebreaks cutting words
-b <- gsub("-\\n", "", a)
-
-# remove begin and end phrases
-b <- gsub("^.*?((Die Sitzung wird.+?eröffnet\\.)|(Beginn:.*?Uhr))\\s{,2}(\\n)*", "\n", b)
-b <- gsub("\\n(((\\(Schlu(ß|ss)(\\sder\\sSitzung):))|(\\(Ende der Sitzung:.*?Uhr\\))).*$", "", b)
-
-"\\n(Schlu(ss|ß)|Ende( der Sitzung)*:.*?$)"
-
-
-# "Beginn?:?\s?(\d){1,2}(\s?[.,]\s?(\d){1,2})?\s?Uhr"
-# "\(Schlu(ß|ss)\s?:?(.*?)\d{1,2}\D+(\d{1,2})?(.*?)\)?|\(Ende der Sitzung: \d{1,2}\D+(\d{1,2}) Uhr\.?\)"
-
-
-
-# remove header
-# b <- gsub("\nDeutscher\\sBundestag.+?\\(.+?\\)\\s\\n", "\n", b)
-# 516 \tDeutscher Bundestag — 18. Sitzung. Bonn, den 24. und 25. November 1949
-# 512 \tDeutscher Bundestag — 18. Sitzung. Bonn, den 24. und 25. November 1949
-
-# remove comments
-b <- gsub("\\n\\(.+?\\)\\s*\\n", "\n", b)
-
-
-# add seperator between speeches
-regex1 <- "((Vizepräsident|Präsident|Frau)(in)*\\s)*" # Leitung
-regex2 <- "(\\s\\(.*?\\)\\s*)*" # Partei
-regex3 <- "(,.{0,1}((Schriftführer)|(Bundeskanzler)|(Bundespräsident)|(Bundesminister.*?))\\s*)*" # Funktion
-
-c <- gsub(paste0("\\n(", regex1, "(D\\.\\s)*(Dr\\.\\s){0,1}(", mdbs_1, ")", regex2, regex3, ":)"), ":_:\\1", b)
-
-d <- data.frame(text = c) %>%
-  mutate(text = str_remove_all(text, "\\n")) %>%
-  separate_rows(text, sep = ":_:") %>%
-  separate(text, into = c("speaker", "text"), sep = ":", extra = "merge")
-
-
-# Die Sitzung wird um 6 Uhr 11 Minuten durch
-# den Präsidenten Dr. Köhler wieder aufgenommen.
-
-
-
-# remove noise: header, page, (A) ...
-b <- gsub("Deutscher.Bundestag.+?[0-9].+?Wahlperiode.+?Sitzung.+?Berlin.+?\\(D\\)", "", a)
-b <- gsub("-\\n", "", b)
-b <- gsub("\\n\\(.*?\\)\\n", " ", b) # remove comments
-
-
-b <- gsub(".*Beginn:.[0-9]+.[0-9]+.Uhr", "", a)
-b <- gsub("\\(Schluss:.[0-9]+.[0-9]+.Uhr.*", "", b)
-
-# add seperator between speeches
-c <- gsub(paste0("(((Vizepräsident|Präsident|Dr\\.)(in)*\\s)*(", mdbs_20, ")[A-Za-zÄÖÜäöü0-9()\\/ ]*?(,.{1,30})*:)(\\n)"), ":_:\\1", b)
-
-
-d <- data.frame(text = c) %>%
-  mutate(text = str_remove_all(text, "\\n")) %>%
-  separate_rows(text, sep = ":_:") %>%
-  separate(text, into = c("speaker", "text"), sep = ":", extra = "merge") %>%
-  filter(speaker != "") %>%
-  separate(speaker, into = c("speaker", "party"), sep = " \\(", fill = "right") %>%
-  mutate(
-    text = str_squish(text),
-    chair = ifelse(str_detect(speaker, "räsident"), TRUE, FALSE),
-    speaker = str_remove(speaker, "(Vizepräsident|Präsident)(in)*\\s"),
-    speaker = str_remove(speaker, ",.*"),
-    party = str_remove(party, "\\)"),
-    speechnum = row_number()
-  ) %>%
-  separate_rows(text, sep = "(?<!Dr)[.!?](?!$)") %>%
-  mutate(
-    text = str_squish(text),
-    row = row_number()
-  )
